b80fcae52e347af7fd75152aac393db9b8f2ea45
Guergeiro <breno-salles@hotmail.com>
2018-06-29 00:37:43 +0100

    Added/changed functions
    Changed functions to check the root_node before using it.
    Added invert_tree function.
    Added search_tree_node function.
    Added number_of_nodes function.
    Added tree_height_functions.

--- libraries/tree/tree.c
+++ libraries/tree/tree.c
@@ -5,6 +5,7 @@
 
 #include "tree.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 
 struct tree *new_tree() {
@@ -17,13 +18,17 @@ void destruct_tree(struct tree *tree) {
 
 void print_tree_ascending_order(struct tree *tree) {
 	if (tree) {
-		print_nodes_ascending_order(tree->root);
+		if (tree->root) {
+			print_nodes_ascending_order(tree->root);
+		}
 	}
 }
 
 void print_tree_descending_order(struct tree *tree) {
 	if (tree) {
-		print_nodes_descending_order(tree->root);
+		if (tree->root) {
+			print_nodes_descending_order(tree->root);
+		}
 	}
 }
 
@@ -36,4 +41,44 @@ void insert_node_tree(struct tree *tree, struct person_node *node) {
 		}
 		tree->n_nodes++;
 	}
+}
+
+void invert_tree(struct tree *tree) {
+	if (tree) {
+		if (tree->root) {
+			invert_node(tree->root);
+		}
+	}
+}
+
+void search_tree_node(struct tree *tree, struct person_node *node) {
+	if (tree && node) {
+		if (tree->root) {
+			struct person_node *result = search_node(tree->root, node);
+			if (result) {
+				print_person(result->data);
+			} else {
+				printf("\nNode doesn't exit");
+			}
+		} else {
+			printf("\nNode doesn't exist");
+		}
+	}
+	destruct_person_node(node);
+}
+
+unsigned int number_of_nodes(struct tree *tree) {
+	if (tree) {
+		return tree->n_nodes;
+	}
+	return 0;
+}
+
+unsigned int tree_height(struct tree *tree) {
+	if (tree) {
+		if (tree->root) {
+			return max_node(tree->root);
+		}
+	}
+	return 0;
 }
--- libraries/tree/tree.h
+++ libraries/tree/tree.h
@@ -23,4 +23,12 @@ void print_tree_descending_order(struct tree *tree);
 
 void insert_node_tree(struct tree *tree, struct person_node *node);
 
+void invert_tree(struct tree *tree);
+
+void search_tree_node(struct tree *tree, struct person_node *node);
+
+unsigned int number_of_nodes(struct tree *tree);
+
+unsigned int tree_height(struct tree *tree);
+
 #endif /* LIBRARIES_TREE_TREE_H_ */
